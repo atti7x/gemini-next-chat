cf3ad7909fb574bf05717f3dba440ddf4e4a0a46
atti7x <xatairan@gmail.com>
2025-08-30 14:53:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,9 +72,7 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" dir="auto" suppressHydrationWarning>
-      <head>
-        {HEAD_SCRIPTS ? <Script id="headscript">{HEAD_SCRIPTS}</Script> : null}
-
+      <head> {HEAD_SCRIPTS ? <Script id="headscript">{HEAD_SCRIPTS}</Script> : null}
         <meta 
     name="viewport" 
     content="width=device-width, initial-scale=1.0, viewport-fit=cover"
