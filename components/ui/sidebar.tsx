b2bc8e0de65c38b364d240e7ff4f271d633e6ff3
atti7x <xatairan@gmail.com>
2025-09-01 21:19:09 +0200

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -210,6 +210,7 @@ const Sidebar = React.forwardRef<
         <div
           data-sidebar="sidebar"
           className="bg-sidebar group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow"
+          style={{ paddingBottom: 'env(safe-area-inset-bottom)' }} // DIESE ZEILE HINZUFÜGEN
         >
           {children}
         </div>
